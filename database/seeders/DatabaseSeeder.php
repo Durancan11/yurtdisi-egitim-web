91dc06fc2c1a69a4a152d17a64407c4e6e7dd5fc
Duran Can Demirezen <cndemirezen11@gmail.com>
2026-06-07 20:10:08 +0300

    Seeder siralamasi duzeltildi

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
  public function run()
 {
     $this->call([
+        UsersTableSeeder::class,
+        
         AnnouncementsTableSeeder::class,
         ProductsTableSeeder::class,
         BalanceRequestsTableSeeder::class,
         ContactMessagesTableSeeder::class,
-        UsersTableSeeder::class,
     ]);
 }
 }
